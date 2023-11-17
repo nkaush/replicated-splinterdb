547d61450a77443c392fe0650f7d3cd1439527bd
neilk3 <neilk3@node3.splinterdb.rasl-pg0.wisc.cloudlab.us>
2023-11-17 16:52:57 +0000

    Add protobuf kvstore setup

--- setup_server.sh
+++ setup_server.sh
@@ -5,4 +5,4 @@ set -e
 apt-get update -y
 apt-get install -y cmake make clang gcc wget curl git \
     zlib1g-dev libgflags-dev libaio-dev libconfig-dev libxxhash-dev \
-    software-properties-common shellcheck yamllint
+    software-properties-common shellcheck yamllint libprotoc-dev rpcsvc-proto
