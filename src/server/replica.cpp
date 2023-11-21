3d7794c79c2d7acb0f232886835fc5016a233336
neilk3 <neilk3@node3.splinterdb.rasl-pg0.wisc.cloudlab.us>
2023-11-21 21:48:04 +0000

    Fix add server mechanism

--- src/server/replica.cpp
+++ src/server/replica.cpp
@@ -180,11 +180,11 @@ std::pair<cmd_result_code, std::string> replica::add_server(
 std::pair<cmd_result_code, std::string> replica::add_server(
     const srv_config& srv_conf_to_add) {
     ptr<raft_result> ret = raft_instance_->add_srv(srv_conf_to_add);
+    auto rv = ret->get();
     if (!ret->get_accepted()) {
         std::cout << "failed to add server: " << ret->get_result_str()
                   << " (rc=" << ret->get_result_code() << ")" << std::endl;
     } else {
-        auto rv = ret->get();
         std::cout << "add_server succeeded [id=" << srv_conf_to_add.get_id()
                   << ", endpoint=" << srv_conf_to_add.get_endpoint() << "]" 
                   << std::endl;
